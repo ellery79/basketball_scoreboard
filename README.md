0ca50142277f34d98096b46ce899d22dd65c7735
Chuen <hcpoon@gmail.com>
2025-11-16 21:55:31 +0800

    first draft of the homework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # basketball_scoreboard
 Solo Project for Scrimba
+
+# Figma design file
+https://www.figma.com/design/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0-1&p=f&t=SdXHvoCVQ4in6RqG-0
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let homescore=document.getElementById("home-score")
+let guestscore=document.getElementById("guest-score")
 
+function addHomeScore(increment) {
+    let score = parseInt(homescore.textContent) + increment
+    homescore.textContent = score
+}
+
+function addGuestScore(increment) {
+    let score = parseInt(guestscore.textContent) + increment
+    guestscore.textContent = score
+}
+
+function reset() {
+    homescore.textContent = 0
+    guestscore.textContent = 0
+}
